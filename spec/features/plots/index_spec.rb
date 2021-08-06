eb737228cc996de7e15a84116f8c9680bca2cac4
deebot10 <dee1jhill@gmail.com>
2021-08-06 08:44:29 -0600

    Pass test for Story1

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -9,42 +9,37 @@ RSpec.describe 'Plot Index' do
     @plot_3 = @garden.plots.create!(number: 12, size: 'S', direction: 'East')  
 
     @plant_1 = @plot_1.plants.create!(name: 'Purple Beauty Sweet Bell Pepper', description: 'Sweet', days_to_harvest: 10)
-    @plant_2 = @plot_1.plants.create!(name: 'Dandi Lions', description: 'Colorful', days_to_harvest: 5)
-    @plant_3 = @plot_1.plants.create!(name: 'Weeds', description: 'Bitter', days_to_harvest: 8)
+    @plant_2 = @plot_2.plants.create!(name: 'Dandi Lions', description: 'Colorful', days_to_harvest: 5)
+    @plant_3 = @plot_3.plants.create!(name: 'Weeds', description: 'Bitter', days_to_harvest: 8)
 
     visit plots_path
   end
 
   describe 'Story1' do
-    #     User Story 1, Plots Index Page
-    # As a visitor
-    # When I visit the plots index page ('/plots')
-    # I see a list of all plot numbers
-    # And under each plot number I see names of all that plot's plants
     it 'displays all the plot ids' do
-      within("plot-#{@plot_1.id}") do
+      within("#plot-#{@plot_1.id}") do
         expect(page).to have_content(@plot_1.number)
       end
 
-      within("plot-#{@plot_2.id}") do
+      within("#plot-#{@plot_2.id}") do
         expect(page).to have_content(@plot_2.number)
       end
 
-      within("plot-#{@plot_3.id}") do
+      within("#plot-#{@plot_3.id}") do
         expect(page).to have_content(@plot_3.number)
       end
     end
 
     it 'displays the plot plants' do
-      within("plot-#{@plot_1.id}") do
+      within("#plot-#{@plot_1.id}") do
         expect(page).to have_content(@plant_1.name)
       end
 
-      within("plot-#{@plot_2.id}") do
+      within("#plot-#{@plot_2.id}") do
         expect(page).to have_content(@plant_2.name)
       end
 
-      within("plot-#{@plot_3.id}") do
+      within("#plot-#{@plot_3.id}") do
         expect(page).to have_content(@plant_3.name)
       end
     end
